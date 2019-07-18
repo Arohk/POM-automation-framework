0b292ea27bdb7358069644f503a33fc903185a2d
NikolayNedyalkov <nikolaynedyalkov9@gmail.com>
2019-07-18 07:36:17 +0300

    Add elements to Home Page

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -14,6 +14,10 @@ public class HomePage extends BasePage {
 
     // ***Elements***
 
+
+    @FindBy(xpath = "//div[@class=\"notice-container\"]/a[@id='privacybanner']")
+    WebElement closeCookieNotice;
+
     // Side menu button locator
     @FindBy(xpath = "//div[@class='myaccount']/button[@class='menu button-myaccount userlogin']")
     WebElement sideMenuButton;
@@ -21,14 +25,43 @@ public class HomePage extends BasePage {
     @FindBy(id = "imysearchstring")
     WebElement searchBar;
 
+    @FindBy(xpath = "//div[@class=\"autoCompleteDropDownContent\"]//a[@data-name=\"Ндк, София\"]")
+    WebElement resultNDKSofia;
+
+    @FindBy(id = "irestaurantN10Q3R7")
+    WebElement aleHouseID;
+
+    @FindBy(xpath = "//span[contains(text(), 'Телешки наденички')]")
+    WebElement mealName;
+
+    @FindBy(xpath = "//*[@id=\"ibasket\"]//button[@class=\"basket__order-button cartbutton-button\"]")
+    WebElement orderButton;
+
+
+
 
     // ***Implementations***
 
     public void LoginWithTestUserAndDoStuff() throws InterruptedException {
-        setPosition(0, 0, 500, 500);
+        setPosition(0, 0, 1500, 1500);
         navigateToUrl("https://www.takeaway.com/bg/");
+
+        if (isElementDisplayed(closeCookieNotice) == true){
+            click(closeCookieNotice);
+        }
+
         click(searchBar);
         writeText(searchBar, "НДК София");
+        click(resultNDKSofia);
+        moveToElementActions(aleHouseID);
+        click(aleHouseID);
+        moveToElementActions(mealName);
+        click(mealName);
+        click(orderButton);
+
+
+
+
         Thread.sleep(4500);
 
     }
